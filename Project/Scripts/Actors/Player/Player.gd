ad5d064061e82650c62f9cc47c4599dcaf895075
Raphael <raphaelrdgss.006@gmail.com>
2024-12-16 18:47:27 -0300

    Configuring the logic and animations of attack, crounch, landing and defense
    Criando as animações de ataque, queda, defesa e agachar. Atualizando os scripts do player para que execute as animações devidamente assim que as teclas são acionadas

--- Project/Scripts/Actors/Player/Player.gd
+++ Project/Scripts/Actors/Player/Player.gd
@@ -7,6 +7,11 @@ var velocity: Vector2
 var jumpCounter: int = 0
 
 var landing: bool = false
+var attacking: bool = false
+var defending: bool = false
+var crounching: bool = false
+
+var can_track_input: bool = true
 
 
 export(int) var speed
@@ -14,29 +19,65 @@ export(int) var jump_speed
 export(int) var player_gravity 
 
 func _physics_process(_delta: float):
-    horizontal_movement_env()
-    vertical_movement_env()
+	horizontal_movement_env()
+	vertical_movement_env()
+	actions_env()
 
-    gravity(_delta)
-    velocity = move_and_slide(velocity, Vector2.UP)
-    player_sprite.animate(velocity)
+	gravity(_delta)
+	velocity = move_and_slide(velocity, Vector2.UP)
+	player_sprite.animate(velocity)
 
 
 func horizontal_movement_env() -> void:
-    var input_direction: float = Input.get_action_strength("right") - Input.get_action_strength("left")
-    velocity.x = input_direction * speed
+	var input_direction: float = Input.get_action_strength("right") - Input.get_action_strength("left")
+
+	if can_track_input == false or attacking:
+		velocity.x = 0
+		return
+
+	velocity.x = input_direction * speed
 
 func vertical_movement_env() -> void:
-    if is_on_floor():
-        jumpCounter = 0
+	if is_on_floor():
+		jumpCounter = 0
 
-    if Input.is_action_just_pressed("jump") and jumpCounter < 2:
-       jumpCounter += 1
-       velocity.y = jump_speed
+	var jump_condition: bool = can_track_input and not attacking
+
+	if Input.is_action_just_pressed("jump") and jumpCounter < 2 and jump_condition:
+	   jumpCounter += 1
+	   velocity.y = jump_speed
+
+func actions_env() -> void:
+	attack()
+	defense()
+	crouch()
+
+func attack() -> void:
+	var attack_condition: bool = not attacking and not crounching and not defending
+	if Input.is_action_just_pressed("attack") and attack_condition and is_on_floor():
+		attacking = true
+		player_sprite.normal_attack = true
+
+func defense() -> void:
+	if Input.is_action_pressed("defend") and is_on_floor() and not crounching:
+		defending = true
+		can_track_input = false
+	elif not crounching:
+		defending = false
+		can_track_input = true
+		player_sprite.shield_off = true
+
+func crouch() -> void:
+	if Input.is_action_pressed("crouch") and is_on_floor() and not defending:
+		crounching = true
+		can_track_input = false
+	elif not defending:
+		crounching = false
+		can_track_input = true
+		player_sprite.crouch_off = true
 
 func gravity(_delta: float) -> void:
-    velocity.y += _delta * player_gravity
+	velocity.y += _delta * player_gravity
 
-    if velocity.y >= player_gravity:
-        velocity.y = player_gravity
-    
+	if velocity.y >= player_gravity:
+		velocity.y = player_gravity
--- Project/Scripts/Actors/Player/PlayerTexture.gd
+++ Project/Scripts/Actors/Player/PlayerTexture.gd
@@ -2,20 +2,69 @@ extends Sprite
 class_name PlayerTexture
 
 export(NodePath) onready var animation = get_node(animation) as AnimationPlayer
+export(NodePath) onready var player = get_node(player) as KinematicBody2D 
+
+var normal_attack: bool = false
+var suffix: String = "Right"
+var shield_off: bool = false
+var crouch_off: bool = false
 
 func animate(direction: Vector2) -> void:
-    verify_position(direction)
-    horizontal_behavior(direction)
+	verify_position(direction)
+
+	if player.attacking or player.defending or player.crounching:
+		action_behavior()
+	elif direction.y != 0:
+		vertical_behavior(direction)
+	elif player.landing == true:
+		animation.play("Landing")
+		player.set_physics_process(false)
+	else:
+		horizontal_behavior(direction)
 
 
 func verify_position(direction: Vector2) -> void:
-    if direction.x > 0:
-        flip_h = false
-    elif direction.x < 0:
-        flip_h = true
+	if direction.x > 0:
+		flip_h = false
+		suffix = "Right"
+	elif direction.x < 0:
+		flip_h = true
+		suffix = "Left"
+
+func action_behavior() -> void:
+	if player.attacking and normal_attack:
+		animation.play("Attack" + suffix)
+	elif player.defending and shield_off:
+		animation.play("Shield")
+		shield_off = false
+	elif player.crounching and crouch_off:
+		animation.play("Crouch")
+		crouch_off= false
+
+func vertical_behavior(direction: Vector2) -> void:
+	if direction.y > 0:
+		player.landing = true
+		animation.play("Fall")
+	elif direction.y < 0:
+		animation.play("Jump")
 
 func horizontal_behavior(direction: Vector2) -> void:
-    if direction.x != 0:
-        animation.play("Run")
-    else:
-        animation.play("Idle")
+	if direction.x != 0:
+		animation.play("Run")
+	else:
+		animation.play("Idle")
+
+
+func _on_animation_finished(anim_name: String):
+	match anim_name:
+		"Landing":
+			player.landing = false
+			player.set_physics_process(true)
+
+		"AttackRight":
+			normal_attack = false
+			player.attacking = false
+
+		"AttackLeft":
+			normal_attack = false
+			player.attacking = false
